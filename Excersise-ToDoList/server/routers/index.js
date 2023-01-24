57f53fe86e640f5b2ac9341341deb95666a8cb98
Krisna Sandy Pribadi <113227273+Metapanzer@users.noreply.github.com>
2023-01-24 21:56:28 +0700

    Update index.js

--- Excersise-ToDoList/server/routers/index.js
+++ Excersise-ToDoList/server/routers/index.js
@@ -1,3 +1,5 @@
-const toDoRouters = require("./toDoRouters");
+//Import toDoRouters
+const toDoRouters = require("./toDoRouters"); //Refer to toDoRouters.js
 
+//Export toDoRouters as module to be imported in main index.js file
 module.exports = { toDoRouters };
