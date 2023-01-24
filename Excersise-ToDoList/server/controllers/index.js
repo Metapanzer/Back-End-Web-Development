8fd8cd41cfb00616cb2277b38e6815f7a0ad72f8
Krisna Sandy Pribadi <113227273+Metapanzer@users.noreply.github.com>
2023-01-24 21:54:52 +0700

    Update index.js

--- Excersise-ToDoList/server/controllers/index.js
+++ Excersise-ToDoList/server/controllers/index.js
@@ -1,3 +1,5 @@
+//Import file controllers
 const toDoControllers = require("./toDoControllers");
 
+//Export file controller as a module to be imported in routers
 module.exports = { toDoControllers };
