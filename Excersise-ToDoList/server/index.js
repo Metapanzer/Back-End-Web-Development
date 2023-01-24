9f79e3192da982e6b057a983e7013f70de588c6b
Krisna Sandy Pribadi <113227273+Metapanzer@users.noreply.github.com>
2023-01-24 21:57:52 +0700

    Update index.js

--- Excersise-ToDoList/server/index.js
+++ Excersise-ToDoList/server/index.js
@@ -1,6 +1,8 @@
-//ExpressJS Setup
+//Import ExpressJS dependencies
 const express = require("express");
 const app = express();
+
+//Parses incoming JSON requests and puts the parsed data in req.body
 app.use(express.json());
 
 //Define PORT and response for default route
@@ -8,11 +10,11 @@ const PORT = 5000;
 app.get("/", (req, res) => {
   res.status(201).send("<h1>Welcome to API<h1>");
 });
-//CORS
+
+//CORS (granting access from different network (front end to back end))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
-
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -20,8 +22,8 @@ app.use((req, res, next) => {
   next();
 });
 
-//Import router for controller
-const { toDoRouters } = require("./routers");
+//Import router for controller from index.js inside routers folder
+const { toDoRouters } = require("./routers"); //refer to index.js in routers folder
 app.use("/todo", toDoRouters);
 
 //Run the API
