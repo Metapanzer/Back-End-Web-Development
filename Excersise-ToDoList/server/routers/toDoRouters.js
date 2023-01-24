a7cfacf7f4a47855bf635184fde86018b620c8a5
Krisna Sandy Pribadi <113227273+Metapanzer@users.noreply.github.com>
2023-01-24 21:56:57 +0700

    Update toDoRouters.js

--- Excersise-ToDoList/server/routers/toDoRouters.js
+++ Excersise-ToDoList/server/routers/toDoRouters.js
@@ -1,10 +1,14 @@
+//Import dependencies
 const express = require("express");
 const Router = express.Router();
 
-const { toDoControllers } = require("../controllers");
-Router.get("/", toDoControllers.showList);
+//import toDoControllers logic
+const { toDoControllers } = require("../controllers"); //refer to index.js inside controllers folder
+//Create each route for different method
+Router.get("/", toDoControllers.showList); //.showlist refer to module.exports inside toDoController.js etc.
 Router.post("/create", toDoControllers.createTask);
 Router.patch("/edit/:id/", toDoControllers.editTask);
 Router.delete("/delete/:id/", toDoControllers.deleteTask);
 
+//Export all router as module to be imported in index.js file inside routers folder
 module.exports = Router;
